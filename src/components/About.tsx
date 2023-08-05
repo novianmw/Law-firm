9cfc2f7d50e1be3e08118deb5d7caa6d4fc2d256
novianmw <novian.mulyoadi.w@gmail.com>
2023-08-05 13:11:56 +0700

    update: responsive design

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,30 +2,16 @@ import { person4 } from "../assets/images/list";
 
 export default function About() {
   return (
-    <div className="flex justify-between flex-row h-[100vh] w-full p-12">
-        <div className="w-1/3">
-            <img className="w-[400px]" src={person4} alt="" />
-        </div>
-        <div className="w-[500px]">
-            <div className="mt-24 mb-12">
-                <p className="text-xl font-normal uppercase text-orange-300">
-                    about lawyer
-                </p>
-                <h1 className="text-4xl">
-                    Istvan Cocron
-                </h1>
-                <p className="text-lg font-normal uppercase">
-                    CLLB Lawyers Cocron, Liebl,Leitz, Braun
-                </p>
+    <div className="w-full h-full">
+        <div className="flex flex-col md:flex-row justify-between md:justify-around items-center px-6 py-12 md:p-12">
+            <div className="w-full flex flex-col items-center">
+                <img src={person4} alt="CEO" className="w-full md:w-[400px] border-2 border-white rounded" />
             </div>
-            <div className="mb-8">
-                <p className=""> the firm was established in 2007 in Jakarta, Indonesia As intellectual property law. Our exposure to the complexities in ambit of intellectual property law give us the impetus spriration to diversity Into other streams. We are experienced in handling international legal matters with empanelled with U.S consulate British Deputy High Commission.</p>
-            </div>
-            <div className="flex flex-row justify-between items-start">
-                <div className="flex flex-col items-start">
-                    <h3 className="text-2xl">Istvan Cocron</h3>
-                    <p className="text-lg">Chairman & CEO</p>
-                </div>
+            <div className="w-full flex flex-col items-start">
+                <h1 className="py-3 text-xl md:text-2xl font-normal text-orange-300 text-center md:text-start uppercase">About Lawyer</h1>
+                <h1 className="pb-1 text-4xl md:text-5xl font-semibold text-black text-center md:text-start">Istan Cocron</h1>
+                <p className="pb-5 text-xl md:text-2xl text-slate-500 text-start uppercase">CLLB Lawyers Cocron, Liebl,Leitz, Braun</p>
+                <p className="pb-5 md:pr-14 text-lg md:text-xl text-black text-justify md:text-start"> the firm was established in 2007 in Jakarta, Indonesia As intellectual property law. Our exposure to the complexities in ambit of intellectual property law give us the impetus spriration to diversity Into other streams. We are experienced in handling international legal matters with empanelled with U.S consulate British Deputy High Commission.</p>
             </div>
         </div>
     </div>
