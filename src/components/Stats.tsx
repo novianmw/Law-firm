6a7307ae018abaae8ec5b201fe9614ce559ca872
novianmw <novian.mulyoadi.w@gmail.com>
2023-08-06 08:14:46 +0700

    update: responsive design

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,19 +1,21 @@
-import { stats } from "../constant"
+import { stats } from '../constant'
 
 export default function Stats() {
     return (
-        <div className="flex item-center h-[50vh] bg-indigo-950">
-            {stats.map((stat) => {
-                return (
-                    <div key={stat.id} className="flex flex-row items-center justify-center w-1/4 text-center text-white">
-                        <div className="flex flex-col items-center justify-center w-full">
-                            <img className="text-white" src={stat.img} alt="" />
-                            <h3 className="font-semibold text-2xl">{stat.value}</h3>
-                            <p className="font-normal text-xl">{stat.title}</p>
+        <div className="w-full h-full">
+            <div className="flex flex-col md:flex-row justify-around item-center bg-indigo-950 px-6 py-12 md:p-6">
+                {stats.map((stat) => {
+                    return (
+                        <div key={stat.id} className="flex flex-row items-center justify-center">
+                            <div className="flex flex-col items-center justify-center w-full py-5">
+                                <img className="text-white w-10 md:w-14 h-10 md:h-14" src={stat.img} alt="" />
+                                <h3 className="text-xl md:text-2xl font-semibold text-white">{stat.value}</h3>
+                                <p className="text-lg md:text-xl font-normal text-white">{stat.title}</p>
+                            </div>
                         </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
         </div>
-  ) 
+    )
 }
